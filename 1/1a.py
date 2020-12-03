84d61c0f5a6ae358dde6ff64b8cf92c03c98c50f
Mckenzie West <westmr@uwec.edu>
2020-12-03 13:27:02 -0600

    Solve second problem

--- 1/1a.py
+++ 1/1a.py
@@ -9,13 +9,14 @@ Created on Thu Dec  3 13:09:49 2020
 def load_and_clean(filename):
     with open(filename) as inputs:
         as_string = inputs.read()
-    int_list = [int(a) for a in as_string.split('\n') if len(a)>0]
+    int_list = [int(a) for a in as_string.strip().split('\n')]
     return int_list
 
 def get_pair_sum(a_list,desired_sum):
     for a in a_list:
         if desired_sum-a in a_list:
             return a
+    return False
 
 def prod_with_diff(a,given_sum):
     return a*(given_sum-a)
@@ -25,4 +26,14 @@ def solve_first(filename="ints.txt",year=2020):
     val = get_pair_sum(int_list,year)
     return prod_with_diff(val,year)
 
-print(solve_first())
+def solve_second(filename="ints.txt",year=2020):
+    int_list = load_and_clean(filename)
+    for a in int_list:
+        b = get_pair_sum(int_list,year-a)
+        if b:
+            c = year-a-b
+            break
+    assert a+b+c == year
+    return a*b*c
+
+print(solve_second())
